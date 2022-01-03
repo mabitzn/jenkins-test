0745762a67e886eb48750e95aa80f12099238943
Paul Lotz <paul.lotz@gmx.at>
2022-01-03 20:42:09 +0100

    check

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,45 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    customLaunchers: {
-      Chrome_without_sandox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
+      require('@angular/cli/plugins/karma')
+    ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    browsers: ['Chrome']
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
+    },
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'coverage-istanbul', 'dots', 'junit']
+              : ['progress', 'kjhtml', 'dots', 'junit'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    junitReporter: {
+      outputFile: 'test-results.xml'
+    },
+    singleRun: false
   });
 };
